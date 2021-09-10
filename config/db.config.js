1d29be05a84244bb07b506ef24b341ebd1fd0acd
LAPTOP-774063JM\ASUS <boatsuban2543@gmail.com>
2021-09-10 15:12:45 +0700

    Add AreaAPI

--- config/db.config.js
+++ config/db.config.js
@@ -4,7 +4,7 @@ const dbConn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'bookshopdb'
+  database : 'specialtopicdb'
 });
 dbConn.connect(function(err) {
   if (err) throw err;
